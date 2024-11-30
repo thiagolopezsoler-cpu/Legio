6fd39f6e1d42e61cc7a96bc4945fa1368d1a0989
Thiagolopezsoler <d49093645@alumnos.itr.edu.ar>
2024-11-30 19:24:45 -0300

    Mejorando por mucho el juego de la busquedad del tesoro

--- react/otroReintentoDeLaBusquedaDelTesoro/src/App.jsx
+++ react/otroReintentoDeLaBusquedaDelTesoro/src/App.jsx
@@ -1,28 +1,70 @@
 import React, { useState } from 'react'
 
-
 export const App = () => {
-  const [jugador, setJugador] = useState('');
-  const [mode, setMode] = useState('preparation')
+  //save of name for player
+  const [player, setPlayer] = useState('Player');
+  //change mode of game
+  const [mode, setMode] = useState('preparation');
+  //is the attempts by player
+  const [attemptsValue, setAttemptsValue] = useState(0);
+  //arrangement with square
+  const [positions, setPositions] = useState(Array(25).fill(null));
+  //number lucky for place from trasure
+  const [locationFromTrasure, setLocationFromTrasure] = useState(7);
 
-  function changeName() {
-    console.log(jugador);
+  function playGame() {
+    console.log(player);
     setMode('game');
+    generationNumber();
+  }
+
+  function generationNumber(){
+    let locationFromTrasure = Math.floor(Math.random()*24);
+    setLocationFromTrasure(locationFromTrasure);
+    console.log(locationFromTrasure);
+
+  }
+  function checkedBox(index) {
+    console.log("the click of the button arrived " + index)
+    if(index === locationFromTrasure){
+      alert("You are THE WINNER, congratulations");
+      setPositions(Array(25).fill(null));
+      generationNumber();
+    } else{
+      const newPositions = [...positions];
+      newPositions[index] = "x"
+      setPositions(newPositions);
+    }
+    setAttemptsValue(attemptsValue + 1);
   }
   return (
     <div>
       {mode === 'preparation' ? (
-        <div>
+        <div className='start'>
           <h1>Welcome to......</h1>
           <input
+          className='input'
             type="text"
             placeholder='how is your name?'
-            onChange={(e) => setJugador(e.target.value)}
+            onChange={(e) => setPlayer(e.target.value)}
           />
-          <button onClick={changeName}>go</button>
+          <button  onClick={playGame} className='btn'>go</button>
         </div>) : (
         <div>
-          <h1>Hello, {jugador}! The game starts now.</h1>
+          <h1>Hello, {player}! The game starts now.</h1>
+          <p>yours attempts {attemptsValue}</p>
+          <div className='bottons'>
+            {positions.map((value, index) => {
+              return(
+              <div
+              className={positions[index] === null ? 'botton': 'bottonPressed'}
+                key={index}
+                onClick={() => checkedBox(index)}>
+                {value}
+              </div>
+              )
+            })}
+          </div>
         </div>
       )}
     </div>
