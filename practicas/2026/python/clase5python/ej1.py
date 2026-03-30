5b59f28a974aa8b937ce05e758d4f10fa9021df8
Thiagolopezsoler <thiagolopezsoler@gmail.com>
2026-03-30 19:06:21 -0300

    dds

---
+++ practicas/2026/python/clase5python/ej1.py
@@ -0,0 +1,4 @@
+# 1- Calculadora de índice de masa corporal (IMC):
+# - Solicita al usuario que ingrese su peso en kg y su altura en metros.
+# - Calcula el IMC utilizando la fórmula: IMC = peso / (altura * altura).
+# - Muestra el IMC calculado y una categoría de peso según el IMC (talla s, talla m, talla l, talla xl).
--- practicas/2026/python/pooconstructor/ej5.py
+++ practicas/2026/python/pooconstructor/ej5.py
@@ -1,2 +1,7 @@
 # Ejercicio 5: Clase Coche + Diccionario y Tupla
 # Objetivo: Crear un diccionario de coches y almacenar sus descripciones en una tupla.
+
+
+class Coche:
+    def __init__(self):
+        
